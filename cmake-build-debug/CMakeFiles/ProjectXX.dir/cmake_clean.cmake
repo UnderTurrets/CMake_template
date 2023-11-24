b5b2d9b4dc88150abeaf391c5d656d6d2788e0ff
xu736946693 <736946693@qq.com>
2023-11-24 21:15:00 +0800

    非常规整

---
+++ cmake-build-debug/CMakeFiles/ProjectXX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectXX.dir/lib/Module1/module1.cpp.o"
+  "libProjectXX.a"
+  "libProjectXX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectXX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
